e8012846414bfd1dfb6fa6fc8f0c11061f86fad5
ashrafffathalla <75317437+ashrafffathalla@users.noreply.github.com>
2022-03-13 12:46:37 +0200

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	 int x = 4;
+     System.out.println(x);
+     //reassign value
+     x = 8;
+     String myName= "Ashraf";
+     System.out.println(x);
+    }
+}
